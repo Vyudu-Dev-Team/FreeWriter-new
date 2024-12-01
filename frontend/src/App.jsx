1b1074d4735bf76cf66c8550cffc60c40caf18b5
Phil Wamba <philwamba@gmail.com>
2024-12-01 15:14:00 +0300

    feat: implement story writing view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,9 +26,13 @@ import PrivateRoute from './components/PrivateRoute';
 
 import FeedbackForm from './components/FeedbackForm';
 
+import MyStoriesScreen from './components/MyStoriesScreen';
+import StoryWritingView from './components/StoryWritingView';
+
 import { AppProvider } from './contexts/AppContext';
 import ProfilePage from './components/profileSetup';
 import TitleScreen from './components/SplashScreen';
+import PromptSelectedPage from './components/PromptSelected';
 
 
 function App() {
@@ -48,12 +52,16 @@ function App() {
             <Route path="/onboarding" element={<PrivateRoute><Onboarding /></PrivateRoute>} />
             <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
             <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/prompt" element={<PromptPage />} />
+            <Route path="/prompt/selected" element={<PromptSelectedPage />} />
             <Route path="/story-map" element={<PrivateRoute><StoryMapComponent /></PrivateRoute>} />
             <Route path="/story-map/:storyId" element={<PrivateRoute><StoryMap /></PrivateRoute>} />
             <Route path="/deck" element={<PrivateRoute><DeckManager /></PrivateRoute>} />
+            <Route path="/write" element={<StoryWritingView />} />
             <Route path="/write/:storyId" element={<PrivateRoute><WritingWorkspace /></PrivateRoute>} />
             {/* <Route path="/notifications" element={<PrivateRoute><NotificationCenter /></PrivateRoute>} /> */}
             <Route path="/badges" element={<PrivateRoute><BadgeDisplay /></PrivateRoute>} />
+            <Route path="/stories" element={<MyStoriesScreen />} />
             <Route path="/sketch" element={<PrivateRoute><SketchGenerator /></PrivateRoute>} />
             <Route path="/feedback" element={<PrivateRoute><FeedbackForm /></PrivateRoute>} />
           </Routes>
