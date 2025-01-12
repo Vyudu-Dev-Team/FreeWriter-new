7c1295d4210484395db94857279b653efd5a9da8
jul10l1r4 <jul10l1r4@disroot.org>
2025-01-12 04:45:00 -0300

    Fix cors

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -33,7 +33,7 @@ const app = express();
 
 // CORS Configuration - Allow all origins
 const corsOptions = {
-  origin: 'http://localhost:5173/',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: false,
