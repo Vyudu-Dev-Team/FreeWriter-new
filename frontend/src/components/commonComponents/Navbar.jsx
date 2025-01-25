60e8ab1e641350ca6bfc66f6b8205aa1054636fc
Brenio Hallison Filho <brenio55@outlook.com>
2025-01-25 13:22:02 -0300

    add: corrected some missing information in the profile page

--- frontend/src/components/commonComponents/Navbar.jsx
+++ frontend/src/components/commonComponents/Navbar.jsx
@@ -5,6 +5,7 @@ import { AppBar, Box, Typography, IconButton, TextField, Paper, Button } from '@
 import SettingsIcon from '@mui/icons-material/Settings';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import { useAppContext } from '../../contexts/AppContext';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const { state, dispatch } = useAppContext();
@@ -54,9 +55,11 @@ function Navbar() {
             <IconButton sx={{ color: '#000',}}>
               <QuestionMarkIcon sx={{border: '1px solid #000', borderRadius: 50, fontSize: '1.4rem'}} />
             </IconButton>
-            <IconButton sx={{ color: '#000' }}>
-              <SettingsIcon sx={{fontSize: '1.4rem'}} />
-            </IconButton>
+            <Link to="/UserPage">
+              <IconButton sx={{ color: '#000' }}>
+                <SettingsIcon sx={{fontSize: '1.4rem'}} />
+              </IconButton>
+            </Link>
           </div>
 				</Box>
 			</Box>
